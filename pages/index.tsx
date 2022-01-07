05d057e9241c1049421c4174a8e2db20787dbf51
Shlok D. Mange <shlok_mange@protonmail.com>
2022-01-07 13:20:49 +0400

    Add Editor to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useCallback, useState } from 'react'
+import Editor from '../src/components/Editor'
 
 const Home: NextPage = () => {
-  return (
+	
+	const [doc, setDoc] = useState<string>('# Hello, World!\n')
+  	const handleDocChange = useCallback(newDoc => {
+    	setDoc(newDoc)
+  	}, [])
+	  
+	return (
 	<div className='h-screen w-screen bg-gray-900'>
 		<Head>
 			<title>Jot - Get paid for writing.</title>
 			<meta name="description" content="Jot is a platform allowing users to get paid in crypto for their articles. A decentralized substack." />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
+		<section>
+			<Editor initialDoc={doc} onChange={handleDocChange}/>
+		</section>
 	</div>
   )
 }
