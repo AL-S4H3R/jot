07832415deb6cc1b68fbc999e025edfed9374e2e
Shlok D. Mange <shlok_mange@protonmail.com>
2022-01-08 22:06:30 +0400

    navbar redesign

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import {  BiSearch, BiMenuAltLeft, BiX } from 'react-icons/bi'
+import {  BiSearch, BiMenuAltLeft, BiX, BiLogIn } from 'react-icons/bi'
 
 const Home: NextPage = () => {
 
@@ -8,24 +8,29 @@ const Home: NextPage = () => {
 
     return(
         <div className='h-screen w-screen' style={{ fontFamily: 'Ubuntu Mono' }} >
-            <nav className='px-4 py-4 flex items-center justify-between'>
-                <h1 className='text-4xl font-bold'>
-                    jot.
-                </h1>
-                <button onClick={() => setIsMenu(!isMenu)}>
-                    { 
-                        isMenu 
-                        ? <BiMenuAltLeft className='text-4xl transition-all duration-200'/> 
-                        : <BiX className='text-4xl transition-all duration-200'/>
-                    }
-                </button>
-                {/* <div className='flex items-center border px-4 py-2'>
-                    <input className='active:border-blue-500' placeholder='Search...'/>
-                    <BsSearch/>
+            <nav className='px-4 py-4 md:flex md:items-center md:justify-between md:px-16'>
+                <div className='flex items-center justify-between'>
+                    <div>
+                        <h1 className='text-4xl font-bold'>
+                            jot.
+                        </h1>
+                    </div>
+                    <div className='md:hidden'>
+                        <button onClick={() => setIsMenu(!isMenu)}>
+                            {!isMenu ? <BiMenuAltLeft className='text-4xl'/> : <BiX className='text-4xl'/>}
+                        </button>
+                    </div>
                 </div>
-                <button className='p-2 border-2 border-black font-semibold tracking-wider'>
-                    Login
-                </button> */}
+                <div className={`${isMenu ? 'block' : 'hidden'} py-4 md:block`}>
+                    <ul className='space-y-4 md:flex md:items-center md:space-y-0 md:space-x-16'>
+                        <li className='justify-center flex'>
+                            <button>Community</button>
+                        </li>
+                        <li className='justify-center flex'>
+                            <button className='px-2 py-1 bg-black text-white'>Connect</button>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
